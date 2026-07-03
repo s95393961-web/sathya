f66f512de0140389666a9ba168524104609c7439
s95393961-web <s95393961@gmail.com>
2026-07-03 21:08:24 +0530

    03-07-2026 Update

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+import Portfolio from './Portfolio.jsx'
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+    <Portfolio></Portfolio>
+    </>
+  )
+}
+
+export default App
---
+++ src/Portfolio.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Portfolio.css'
+
+function Portfolio() {
+  return (
+    // this is html tag in the jsx file for create HTML elements
+    <div>
+         <div className='top'>
+            <p>Welcome to my Portfolio</p>
+         </div>
+        < div className='bottom'>
+        </div>
+
+    </div>
+  )
+}
+
+export default Portfolio
