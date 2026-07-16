7a591b56f439a9c9a851b14c27c69a7f5ab8d484
s95393961-web <s95393961@gmail.com>
2026-07-16 19:25:37 +0530

    16-07-2026 update

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -8,6 +8,20 @@ function Portfolio() {
          <div className='top'>
             <p>Welcome to my Portfolio</p>
          </div>
+         <div className='profile'>
+          <h2>Suriya Prakash G N</h2>
+          <p>B.com</p>
+          <h3>Frontend Developer</h3>
+          <div className= 'links'>
+            <p>instagram id <br /></p>
+            
+            <p>github id <br /></p>
+       
+            <p>linkedin id <br /></p>
+            
+            <p>gmail id <br /></p>
+          </div>
+         </div>
         < div className='bottom'>
         </div>
 
