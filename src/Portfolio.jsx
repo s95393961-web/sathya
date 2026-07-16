7c5db78c4b9e989214b1d01237f722d0bae85ddb
s95393961-web <s95393961@gmail.com>
2026-07-16 20:55:46 +0530

    profile

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -12,15 +12,29 @@ function Portfolio() {
           <h2>Suriya Prakash G N</h2>
           <p>B.com</p>
           <h3>Frontend Developer</h3>
-          <div className= 'links'>
-            <p>instagram id <br /></p>
-            
-            <p>github id <br /></p>
-       
-            <p>linkedin id <br /></p>
-            
-            <p>gmail id <br /></p>
+          <div className='links'>
+            <div className='L1'>
+              <div className='LS5'><img src="https://cdn-icons-png.flaticon.com/512/1384/1384014.png" alt="img" /></div>
+              <h3>Surya Prakash Prakash</h3>
+            </div>
+            <div className='L2'>
+              <div className='LS6'><img src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_640.png" alt="" /></div>
+              <h3>s95393961-web</h3>
+            </div>
+            <div className='L3'>
+              <div className='LS7'><img src="https://cdn-icons-png.flaticon.com/512/3178/3178158.png" alt="" /></div>
+              <h3>s95393961@gmail.com</h3>  
+            </div>
+            <div className='L4'>
+              <div className='LS8'><img src="https://illustoon.com/photo/7817.png" alt="" /></div>
+              <h3>+91 8248166686</h3>
+            </div>   
+
           </div>
+          <div className='picture'>
+
+          </div>
+          
          </div>
         < div className='bottom'>
         </div>
