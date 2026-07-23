1b04a22358946b76f9184d16e2044aa2d6017ba8
s95393961-web <s95393961@gmail.com>
2026-07-23 09:44:42 +0530

    23.07.2026 update

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -5,39 +5,52 @@ function Portfolio() {
   return (
     // this is html tag in the jsx file for create HTML elements
     <div>
+        <div className='Portfolio'>
+            <img src="https://images.openai.com/static-rsc-4/Ua2QKiHQa9XQwh60qzXRuxkvTig3dQitsOlXEcnWFuMKoAOyb5SJcAGhne9RLifgV_Ab2hEjkjg8bg0nkJIdQyUPRmZ4IjxWUsUp4SZfjTRyoUOvCEdrlKq7BVytLk6POVKP3UnuRSRVq4V1JBh8oBF-E8Vq2CjKZAWq9ul7VnlvjcfCDteL3JiEkKKFTHYr?purpose=fullsize" alt="suriya" srcset="" id='bg'></img>
+
          <div className='top'>
             <p>Welcome to my Portfolio</p>
          </div>
-         <div className='profile'>
-          <h2>Suriya Prakash G N</h2>
-          <p>B.com</p>
-          <h3>Frontend Developer</h3>
-          <div className='links'>
-            <div className='L1'>
-              <div className='LS5'><img src="https://cdn-icons-png.flaticon.com/512/1384/1384014.png" alt="img" /></div>
-              <h3>Surya Prakash Prakash</h3>
-            </div>
-            <div className='L2'>
-              <div className='LS6'><img src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_640.png" alt="" /></div>
-              <h3>s95393961-web</h3>
-            </div>
-            <div className='L3'>
-              <div className='LS7'><img src="https://cdn-icons-png.flaticon.com/512/3178/3178158.png" alt="" /></div>
-              <h3>s95393961@gmail.com</h3>  
-            </div>
-            <div className='L4'>
-              <div className='LS8'><img src="https://illustoon.com/photo/7817.png" alt="" /></div>
-              <h3>+91 8248166686</h3>
-            </div>   
+         <div className="profile">
+    <label for="name and degree">Suriya Prakash <u>B.com</u></label>
+<label id="course" for="course">Frontend Developer</label>
+<table>
+    <tr>
+        <td><img src="https://static.vecteezy.com/system/resources/previews/018/930/413/non_2x/instagram-logo-instagram-icon-transparent-free-png.png" alt="Instagram" className="img"></img></td>
+        <td>js_007king</td>
+    </tr>
+<tr>
+        <td><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSp5Tzn4ookR8nWB1FnR1MydV3yjiA8xeNOpvcrbQJq1MxUzpBpdeDc9J0&s=10" alt="Instagram" className="img1"></img></td>
+        <td>Suriya Prakash Prakash</td>
+    </tr>
+<tr>
+        <td><img src="https://cdn.pixabay.com/photo/2022/01/30/13/33/github-6980894_640.png" alt="Instagram" className="img2"></img></td>
+        <td>s95393961-web</td>
+    </tr>
+<tr>
+        <td><img src="https://img.magnific.com/premium-vector/mail-icon-design_996135-41118.jpg?semt=ais_hybrid&w=740&q=80" alt="Instagram" className="img3"></img></td>
+        <td>s95393961@gmail.com</td>
+    </tr>
+    <tr> 
+        <td><img src="https://media.istockphoto.com/id/971654072/vector/red-call-icon.jpg?s=612x612&w=0&k=20&c=bwlNm0pnNs98evZv4x8N3Cq3XQAWIKLEzJPmQpbMgWY=" alt="Phone" className="img4"></img></td>
+        <td>+91 8248166686</td>
+     </tr>
+</table>
+<div id="img">
+    <img src="IMG_20250828_123015_355.jpg" alt="suriya" id="i"></img>
+    </div>
+        <button id="btn">Resume</button>
+</div>
 
-          </div>
-          <div className='picture'>
+<div className="summary">
+    
+</div>
 
-          </div>
           
-         </div>
-        < div className='bottom'>
-        </div>
+          
+  < div className='bottom'>
+ </div>
+ </div>
 
     </div>
   )
